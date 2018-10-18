382137b3c85e4410c6e2dcd4547a451dcb0f925e
JalapenosBud <Jalapenosbud@gmail.com>
2018-10-18 21:44:07 +0200

    ok so it was cause i flipped the boolean it stopped ssending IMAV lol

--- src/com/company/Server/ClientHandler.java
+++ src/com/company/Server/ClientHandler.java
@@ -50,8 +50,6 @@ public class ClientHandler extends Thread {
                 {
                     switch (splitFirst(received))
                     {
-                        
-                        
                         case "JOIN":
                             System.out.println("received a join message");
                             try {
@@ -72,18 +70,22 @@ public class ClientHandler extends Thread {
                             }
                             else if(tmpInfo[2].contains("QUIT"))
                             {
-                                System.out.println("user wants to quit, dc'ing them..");
-                                System.out.println("list size before: " + ServerMain.clients.size());
-                                ServerMain.removeAndUpdateList(userName);
                                 
-                                System.out.println("list size now: " + ServerMain.clients.size() + " user " + userName +" has been removed.");
-                                
-                                sendToAllUsers(showAllClients());
                                 try {
+                                    System.out.println("user wants to quit, dc'ing them..");
+                                    System.out.println("list size before: " + ServerMain.clients.size());
+                                    ServerMain.removeAndUpdateList(userName);
+    
+                                    System.out.println("list size now: " + ServerMain.clients.size() + " user " + userName +" has been removed.");
+    
+                                    sendToAllUsers(showAllClients());
                                     clientSocket.close();
+                                    break;
+                                    
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                 }
+                               
                             }else
                             {
                                 sendToAllUsers(tmpInfo[2]);
@@ -94,7 +96,7 @@ public class ClientHandler extends Thread {
         
                             String[] duoArr = received.split(" ");
                             System.out.println(duoArr[1] + " is alive");
-                            received = "";
+                            //received = "";
                             break;
                     }
                 }
@@ -178,111 +180,4 @@ public class ClientHandler extends Thread {
             System.out.println("J_OK sent\nUser " + userName + " joined.");
         }
     }
-    
-/*private Scanner input;
-private PrintWriter output;
-    
-    
-    private void sendToAllUsers(String message)
-    {
-        if(ClientListManager.getInstance().getSize() <= 1)
-            return;
-    
-        try {
-            for (Client c : ClientListManager.getInstance().getClients())
-            {
-                output = new PrintWriter(c.getSocket().getOutputStream(), true);
-                output.println(c.getName() + ": " + message);
-    
-                System.out.println("sending to: " + c.getName() + " listening on: " + c.getSocket());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    @Override
-    public void run() {
-        do {
-            //string to check what protocol is incoming
-            try{
-                incoming = input.nextLine();
-                //saving the full string for further usage, ie splitting up the message into parts, for specific user name or message element
-                
-            }
-            catch (NoSuchElementException ioe)
-            {
-                System.out.println(ioe);
-            }
-            String saveString = incoming;
-            
-            switch (splitFirst(incoming))
-            {
-                case "JOIN":
-                    System.out.println("received a join message");
-                    try {
-                        checkIfUserJoins(saveString);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case "DATA":
-                    String[] tmpInfo = StringUtilities.splitDataProtocol(incoming);
-                    sendToAllUsers(tmpInfo[2]);
-                    
-                case "LIST":
-                    output.println(ClientListManager.getInstance().showAllClients());
-                    break;
-            }
-        }while (!incoming.equals("CLOSE_SERVER"));
-        
-        try {
-            if (socket != null) {
-                System.out.println("Closing down connection…");
-                socket.close();
-            }
-        } catch (IOException ioEx) {
-            System.out.println("Unable to disconnect!");
-        }
-    }
-    
-    private void checkIfUserJoins(String incoming) throws IOException {
-        String[] tmpInfo = splitJoinProtocol(incoming);
-        System.out.println("name is : " + tmpInfo[1] + " before checking JOIN message");
-        
-        Client tmpClient = null;
-        
-        if(ClientListManager.getInstance().getSize() == 0)
-        {
-            System.out.println("size is in == 0" + ClientListManager.getInstance().getSize());
-            tmpClient = returnNewClient(incoming);
-            
-            ClientListManager.getInstance().addToList(tmpClient);
-            System.out.println("\nNow added" + tmpClient.getName());
-            //tryToAddUser(incoming);
-            output.println("J_OK");
-            System.out.println("J_OK sent");
-            
-        }
-        //if there already are people on the server
-        else if(ClientListManager.getInstance().getSize() > 0)
-        {
-            tmpClient = returnNewClient(incoming);
-            
-            for(int i = 0; i < ClientListManager.getInstance().getSize(); i++)
-            {
-                //get name of clients and check if exists
-                //if user name exists
-                if(ClientListManager.getInstance().getClient(i).getName().equalsIgnoreCase(tmpClient.getName()))
-                {
-                    output.println("J_ERR");
-                    //set to false and start loop over?
-                    System.out.println(tmpClient.getName() + " already exists on server\n");
-                    break;
-                }
-            }
-            ClientListManager.getInstance().addToList(tmpClient);
-            output.println("J_OK");
-        }
-    }*/
 }
