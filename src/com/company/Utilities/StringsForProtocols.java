ee356934960a6774ce85aa9b3872700143fffce3
JalapenosBud <Jalapenosbud@gmail.com>
2018-10-22 22:42:09 +0200

    updated code with faisal static string standards

--- src/com/company/Utilities/StringsForProtocols.java
+++ src/com/company/Utilities/StringsForProtocols.java
@@ -6,4 +6,7 @@ public class StringsForProtocols
     public static String IMAV = "IMAV";
     public static String QUIT = "QUIT";
     public static String DATA = "DATA";
+    public static String J_OK = "J_OK";
+    public static String J_ERR = "J_ERR";
+    public static String LIST = "LIST";
 }
